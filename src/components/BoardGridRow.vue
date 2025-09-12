473eb313e01f579ce9229561e520acf4c0907d2a
Chris Irwin <pianowow@gmail.com>
2025-09-12 15:28:16 -0400

    simplify

--- src/components/BoardGridRow.vue
+++ src/components/BoardGridRow.vue
@@ -1,19 +1,12 @@
 <script setup>
-   import { computed, defineProps } from 'vue';
+   import { defineProps } from 'vue';
    import BoardGridRowTile from './BoardGridRowTile.vue';
    const props = defineProps(['row']);
-   const letters = computed(() => {
-      let arr = [];
-      for (let i = 0; i < 5; i++) {
-         arr.push(props.row.slice(i, i + 1));
-      }
-      return arr;
-   });
 </script>
 
 <template>
    <div class="row">
-      <BoardGridRowTile v-for="(letter, index) in letters" :key="index" :letter="letter" />
+      <BoardGridRowTile v-for="(letter, index) in props.row" :key="index" :letter="letter" />
    </div>
 </template>
 
