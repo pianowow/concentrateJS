4719cd0edb82b09df6eaa3a96a0786e69459c441
Chris Irwin <pianowow@gmail.com>
2025-10-15 15:00:13 -0400

    first theme

---
+++ src/js/board.js
@@ -0,0 +1,13 @@
+export class LightColorTheme {
+   defaultColor = 'rgb(233,232,229)';
+   defaultColor2 = 'rgb(230,229,226)';
+   blue = 'rgb(120,200,245)';
+   red = 'rgb(247,153,141)';
+   defendedBlue = 'rgb(0,162,255)';
+   defendedRed = 'rgb(255,67,46)';
+   defaultText = 'rgb(46,45,45)';
+   blueText = 'rgb(24,40,49)';
+   defendedBlueText = 'rgb(0,32,51)';
+   redText = 'rgb(49,30,28)';
+   defendedRedText = 'rgb(51,13,9)';
+}
