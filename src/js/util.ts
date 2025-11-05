c5e5fd99d2659cdeab0e1b76339b5903d230e80b
Chris Irwin <pianowow@gmail.com>
2025-11-05 13:38:52 -0500

    initial tooling add, conversion of util

--- src/js/util.ts
+++ src/js/util.ts
@@ -1,9 +1,9 @@
-export function roundTo(num, decimalPlaces) {
+export function roundTo(num: number, decimalPlaces: number) {
    const factor = Math.pow(10, decimalPlaces);
    return Math.round(num * factor) / factor;
 }
 
-export function assert(condition, message = 'Assertion failed') {
+export function assert(condition: boolean, message = 'Assertion failed') {
    if (!condition) {
       throw new Error(message);
    }
